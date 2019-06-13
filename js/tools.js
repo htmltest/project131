dd07590ae47f6c2c60441c755e869273a4946176
htmltest <struev.vs@gmail.com>
2019-06-13 17:39:25 +0300

    updates

--- js/tools.js
+++ js/tools.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
         var curBlock = $($(this).attr('href'));
         if (curBlock.length > 0) {
             $('html, body').animate({'scrollTop': curBlock.offset().top});
+        } else {
+            $('html, body').animate({'scrollTop': 0});
         }
         e.preventDefault();
     });
 
     var item1wave = $('#item1wave').wavify({
         bones: 3,
-        amplitude: 40,
+        amplitude: 100,
         color: 'url(#wave-gradient1)',
         speed: .25,
         container: '#realme3pro-wave'
@@ -18,7 +20,7 @@ $(document).ready(function() {
 
     var item2wave = $('#item2wave').wavify({
         bones: 3,
-        amplitude: 40,
+        amplitude: 100,
         color: 'url(#wave-gradient2)',
         speed: .25,
         container: '#realme3-wave'
@@ -26,7 +28,7 @@ $(document).ready(function() {
 
     var item3wave = $('#item3wave').wavify({
         bones: 3,
-        amplitude: 40,
+        amplitude: 100,
         color: 'url(#wave-gradient3)',
         speed: .25,
         container: '#realmec2-wave'
@@ -42,10 +44,12 @@ $(window).on('load resize scroll', function() {
     var curScroll = $(window).scrollTop();
     var curHeight = $(window).height();
 
+    $('nav li').removeClass('active');
+    $('nav li').eq(0).addClass('active');
     $('nav a').each(function(e) {
         var curLink = $(this);
         var curBlock = $(curLink.attr('href'));
-        if ((curScroll + curHeight / 2) > curBlock.offset().top) {
+        if (curBlock.length > 0 && ((curScroll + curHeight / 2) > curBlock.offset().top)) {
             $('nav li.active').removeClass('active');
             curLink.parent().addClass('active');
         }
